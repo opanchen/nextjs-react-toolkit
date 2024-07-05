69a61db1692193a1203eaa75dd61755b1e79c58c
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-07-05 13:05:04 +0200

    initial version & commented additional one

---
+++ index.js
@@ -0,0 +1,20 @@
+// TODO: To make initial open card effect
+
+// const image = document.querySelector(".selected");
+// const images = document.querySelectorAll(".image");
+
+// images.forEach((el) => {
+//   el.addEventListener("mouseover", (e) => {
+//     if (e.target.value !== image) {
+//       image.classList.remove("selected");
+//     }
+//   });
+
+//   el.addEventListener("mouseleave", (e) => {
+//     if (e.target.value !== image) {
+//       if (!image.classList.contains("selected")) {
+//         image.classList.add("selected");
+//       }
+//     }
+//   });
+// });
